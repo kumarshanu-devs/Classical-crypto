a2df9bad17bc7528e94abc78d1bcb0e3abe8f904
Kumar Shanu Sarkar <kumar2002shanu@gmail.com>
2025-06-07 14:33:08 +0600

    Add Caesar cipher implementation
    This script implements the Caesar cipher with basic shift encryption and decryption.

---
+++ CaesarCipher/caesar.py
@@ -0,0 +1,18 @@
+def caesar_encrypt(text, shift):
+    result = ''
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            result += char
+    return result
+
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
+
+# Example
+msg = "HELLO"
+enc = caesar_encrypt(msg, 3)
+print("Encrypted:", enc)
+print("Decrypted:", caesar_decrypt(enc, 3))
